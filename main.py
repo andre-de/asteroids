83b27eb0eda2a0b79edc2a5b4e5aae645f227f01
andre-de <neubertandre@yahoo.de>
2024-11-11 15:55:31 +0100

    save

--- main.py
+++ main.py
@@ -17,16 +17,21 @@ def main():
 
     while True:
 
+        dt = clock.tick(60) / 1000
+
         #close the window when i click X
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0, 0, 0))
+        
+        player.update(dt)
         player.draw(screen)
+        
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
+        
 
 
 
