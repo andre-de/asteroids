d680d8ce0a5c7ca2b3f08581d403e42ec027c703
andre-de <neubertandre@yahoo.de>
2024-11-11 10:14:49 +0100

    save

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+
+
+#only run main when the file is run directly, not when it is imported as a module
+if __name__ == "__main__":
+    main()
+
+
